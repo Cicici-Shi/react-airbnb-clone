15380c65d57a2432420d230bcbc7c320d70cbae9
Cici <1901177100@qq.com>
2024-03-11 16:15:11 +0800

    update readme

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://codercba.com:1888/airbnb/api'
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = 'http://120.25.231.132:3001'
 export const TIMEOUT = 100000
